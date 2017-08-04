8e082bc6da9337530c8130751f37cec2158ef5f6
Phillip Demro <phillip.demro@hotmail.com>
2017-08-04 15:59:27 -0400

    removed dist folder. Can get username but graph calls don't work

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 import { Authentication, AdalConfig } from 'adal-ts';
 import { AppState } from '../app.service';
 import { Title } from './title';
 import { XLarge } from './x-large';
+
 
 @Component({
   // The selector is what angular internally uses
@@ -24,54 +26,96 @@ export class HomeComponent {
   disableLogout: boolean = false;
   disableLogin: boolean = false;
 
-  constructor(public appState: AppState, public title: Title) {
+  constructor(public appState: AppState, public title: Title, public http: Http) {
 
   }
 
   ngOnInit() {
     console.log('hello `Home` component');
-    
   }
 
- 
+
   login() {
     console.log('login');
-    
+
     let context = Authentication.getContext(this.createConfig());
     this.disableLogin = context.getUser() != null;
-    
-    if(this.disableLogin == false)  context.login();
+
+    if (this.disableLogin == false) context.login();
 
   }
 
   logout() {
     console.log('logout');
-    
+
     let context = Authentication.getContext(this.createConfig());
-    let loggedInUser = context.getUser(); 
+    let loggedInUser = context.getUser();
     this.disableLogout = loggedInUser == null;
 
-    if(this.disableLogout == false)  context.logout();
+    if (this.disableLogout == false) context.logout();
 
   }
 
-  getUser() :void {
+  getUser(): void {
     console.log('logout');
-    
+
     let context = Authentication.getContext(this.createConfig());
     this.user = context.getUser();
 
+    console.log(context.getToken());
+
+    // this.http.get('https://httpbin.org/get').subscribe(res => {
+    //   console.log(res)
+    // })
+
+    var headers = new Headers();
+    headers.append('Authorization', `"Bearer ${context.getToken()}"`)
+    headers.append('accept', 'application/json, text/plain, */*')
+
+    this.http.get('https://graph.microsoft.com/v1.0/users/pdemro@SPE372945.onmicrosoft.com',
+      {
+        headers: headers
+      }
+    ).subscribe(res => {
+      console.log('blow up');
+    });
   }
   //pdemro@SPE372945.onmicrosoft.com
 
-  private createConfig() :AdalConfig {
-    let config: AdalConfig = {
-      tenant: 'SPE372945.onmicrosoft.com',
-      clientId: '71ab1320-91d8-414e-a925-2e29a6624812',
-      postLogoutRedirectUrl: window.location.origin + '/',
-      //postLogoutRedirectUri: window.location.origin + '/',
-      redirectUri: window.location.origin + '/'
-    };
-    return config;
-  }
+private createConfig(): AdalConfig {
+  let config = new AdalConfig('71ab1320-91d8-414e-a925-2e29a6624812', 'SPE372945.onmicrosoft.com', 'http://localhost:3000/')
+
+  config.extraQueryParameter = 'resource=https://graph.microsoft.com';
+
+  return config;
+
+}
+
+  // private createConfig(): AdalConfig {
+  //   let config: AdalConfig = {
+  //     tenant: 'SPE372945.onmicrosoft.com',
+  //     clientId: '71ab1320-91d8-414e-a925-2e29a6624812',
+  //     postLogoutRedirectUrl: window.location.origin + '/',
+  //     //postLogoutRedirectUri: window.location.origin + '/',
+  //     redirectUri: window.location.origin + '/',
+  //     responseType: '',
+  //     extraQueryParameter: 'resource=https://graph.microsoft.com',
+  //     resource: ''
+  //   };
+  //   return config;
+  // }
+
+// // Create config and get AuthenticationContext
+// window.config = {
+//   tenant: variables.azureAD,
+//   clientId: variables.clientId,
+//   postLogoutRedirectUri: window.location.origin,
+//   endpoints: {
+//     graphApiUri: "https://graph.microsoft.com",
+//     sharePointUri: "https://" + variables.sharePointTenant + ".sharepoint.com",
+//   },
+//   cacheLocation: "localStorage"
+// };
+// var authContext = new AuthenticationContext(config);
+
 }
